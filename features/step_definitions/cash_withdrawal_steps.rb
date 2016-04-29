f1aeec7f6f7f811a39661f2d16e84465bee391f7
Jared <jaredcmurphy@gmail.com>
2016-04-29 15:13:23 -0400

    adds first feature and steps files

---
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -0,0 +1,11 @@
+Given(/^I have deposited \$(\d+) in my account$/) do |amount|
+    pending("Need to design account interface")
+end
+
+When(/^I request \$(\d+)$/) do |amount|
+    pending('How to simulate cash being requested')
+end
+
+Then(/^\$(\d+) should be dispensed$/) do |amount|
+    pending('How to validate that cash was dispensed')
+end
