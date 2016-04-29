2f9d7bdc681ba4d265bd9bb413ab1fdccdfb017b
Jared <jaredcmurphy@gmail.com>
2016-04-29 15:42:58 -0400

    adds transform method to turn numbers into integers

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -11,16 +11,21 @@ class Account
     end
 end
 
-Given(/^I have deposited \$(\d+) in my account$/) do |amount|
-    my_account = Account.new
-    my_account.deposit(amount.to_i)
-    my_account.balance.should eq(amount.to_i)
+CAPTURE_A_NUMBER = Transform /^\d+$/ do |number|
+    number.to_i
 end
 
-When(/^I request \$(\d+)$/) do |amount|
+Given(/^I have deposited \$(#{CAPTURE_A_NUMBER}) in my account$/) do |amount|
+    my_account = Account.new
+    my_account.deposit(amount)
+    my_account.balance.should eq(amount)
+        "Expected the balance to be #{amount} but it was #{my_account.balance}"
+end
+
+When(/^I request \$(#{CAPTURE_A_NUMBER})$/) do |amount|
     pending('How to simulate cash being requested')
 end
 
-Then(/^\$(\d+) should be dispensed$/) do |amount|
+Then(/^\$(#{CAPTURE_A_NUMBER}) should be dispensed$/) do |amount|
     pending('How to validate that cash was dispensed')
 end
