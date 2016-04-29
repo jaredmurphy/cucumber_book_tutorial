4cb70e10c9b9ced8367297388c576d1db7990604
Jared <jaredcmurphy@gmail.com>
2016-04-29 16:08:05 -0400

    second step passes with world module mixin

--- features/step_definitions/cash_withdrawal_steps.rb
+++ features/step_definitions/cash_withdrawal_steps.rb
@@ -1,10 +1,4 @@
-module KnowsMyAccount
-    def my_account
-        @my_account ||= Account.new
-    end
-end
 
-World(KnowsMyAccount)
 
 class Account
     def initialize
@@ -28,16 +22,23 @@ CAPTURE_CASH_AMOUNT = Transform /^\d+$/ do |number|
     number.to_i
 end
 
+module KnowsMyAccount
+    def my_account
+        @my_account ||= Account.new
+    end
+end
+
+World(KnowsMyAccount)
+
 Given(/^I have deposited \$(#{CAPTURE_CASH_AMOUNT}) in my account$/) do |amount|
-    @my_account = Account.new
-    @my_account.deposit(amount)
-    @my_account.balance.should eq(amount),
+    my_account.deposit(amount)
+    my_account.balance.should eq(amount),
         "Expected the balance to be #{amount} but it was #{my_account.balance}"
 end
 
 When(/^I withdraw \$(#{CAPTURE_CASH_AMOUNT})$/) do |amount|
     teller = Teller.new
-    teller.withdraw_from(@my_account, amount)
+    teller.withdraw_from(my_account, amount)
 end
 
 Then(/^\$(#{CAPTURE_CASH_AMOUNT}) should be dispensed$/) do |amount|
