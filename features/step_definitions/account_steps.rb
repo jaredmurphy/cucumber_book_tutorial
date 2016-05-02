f8179dc3690f2140e17470aadaf8f60d732cd9af
Jared <jaredcmurphy@gmail.com>
2016-05-02 15:55:59 -0400

    refactored code, about to build sinatra app

--- features/step_definitions/account_steps.rb
+++ features/step_definitions/account_steps.rb
@@ -1,8 +1,6 @@
 
 Given /^I have deposited (#{CAPTURE_CASH_AMOUNT}) in my account$/ do |amount|
-  my_account.deposit(amount)
-  my_account.balance.should eq(amount), 
-    "Expected the balance to be #{amount} but it was #{my_account.balance}"
+  my_account.credit(amount)
 end
 
 And(/^the balance of my account should be (#{CAPTURE_CASH_AMOUNT})$/) do |balance|
